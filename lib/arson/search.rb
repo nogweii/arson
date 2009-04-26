d3bc26f132c585045bbb978ab0d7ccc987ebe33c
Colin Shea <evaryont@saphrix.com>
2009-04-26 17:26:12 -0400

    Retabbed, added Arson.find_exact

--- lib/arson/search.rb
+++ lib/arson/search.rb
@@ -1,38 +1,46 @@
 class Arson
-	def search(*args)
-	keywords = args.join(' ')
-	if keywords == ""
-		puts "Please specifiy what to search for as the parameters to this script"
-		puts "e.g. #{PROGRAM} pacman"
-		exit 1
-	end
-	json = open("http://aur.archlinux.org/rpc.php?type=search&arg=#{URI.escape(keywords)}").read
-	hash = JSON.parse(json)
-	# The most important piece of the code!
-	if hash["type"] == "search"
-		# Alt search orders: [pkg['CategoryID'],pkg['Name']] - ID num, then name
-		#                    [Categories[pkg['CategoryID'].to_i],pkg['Name']] - Category name, then pkg name
-		hash["results"].sort_by{|pkg|[pkg['Name']]}.each do |pkg|
-			next if File.exists? "/var/lib/pacman/sync/community/#{pkg['Name']}-#{pkg['Version']}" # Dir["/var/lib/pacman/sync/community/#{pkg['Name']}-*"].first
-			p pkg if $VERBOSE
-			name = colorful("White", pkg['Name'])
-			name = colorful("Red", name) if pkg["OutOfDate"] == "1"
-			if Categories[pkg['CategoryID'].to_i] && Categories[pkg['CategoryID'].to_i] != "nil"
-				category = "-#{Categories[pkg['CategoryID'].to_i]}"
-			else
-				category = ""
-			end
-			puts <<HERE
-#{colorful("Magenta", "aur#{category}")}/#{name} #{colorful("Green",pkg['Version'])}
-	#{pkg['Description']}
-HERE
-			$stdout.flush
-		end
-	end
+	class << self
+		# Given an array of search parameters, return an array of hashes
+		# describing each package search result.
+		def search(*args)
+			keywords = args.join(' ')
+			json = open("http://aur.archlinux.org/rpc.php?type=search&arg=#{URI.escape(keywords)}").read
+			hash = JSON.parse(json)
+			packages = []
 
-	if File.exists? "/usr/bin/pacman-color"
-		exec "/usr/bin/pacman-color -Ss #{keywords}"
-	else
-		exec "/usr/bin/pacman -Ss #{keywords}"
+			if hash["type"] == "search"
+
+				# Alt search orders: [pkg['CategoryID'],pkg['Name']] - ID num, then name
+				#                    [Categories[pkg['CategoryID'].to_i],pkg['Name']] - Category name, then pkg name
+				hash["results"].sort_by{|pkg|[pkg['Name']]}.each do |pkg|
+					# Dir["/var/lib/pacman/sync/community/#{pkg['Name']}-*"].first
+					next if File.exists? "/var/lib/pacman/sync/community/#{pkg['Name']}-#{pkg['Version']}"
+					p pkg if $VERBOSE
+					packages << pkg
+				end
+			end
+
+			packages
+		end
+
+		# Attempts to find an exact matching package in AUR
+		def find_exact(arg)
+			hash = JSON.parse(open("http://aur.archlinux.org/rpc.php?type=search&arg=#{URI.escape(arg)}").read)
+			packages = []
+
+			if hash["type"] == "search"
+
+				# Alt search orders: [pkg['CategoryID'],pkg['Name']] - ID num, then name
+				#                    [Categories[pkg['CategoryID'].to_i],pkg['Name']] - Category name, then pkg name
+				hash["results"].sort_by{|pkg|[pkg['Name']]}.each do |pkg|
+					# Dir["/var/lib/pacman/sync/community/#{pkg['Name']}-*"].first
+					next if File.exists? "/var/lib/pacman/sync/community/#{pkg['Name']}-#{pkg['Version']}"
+					p pkg if $VERBOSE
+					return package if package['Name'] == arg
+				end
+			end
+
+			return nil
+		end
 	end
 end
