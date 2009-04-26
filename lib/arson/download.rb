e3e81373225f724d5fbdbfa1155586342eaa5a5b
Colin Shea <evaryont@saphrix.com>
2009-04-26 17:25:27 -0400

    Class level methods and fleshing out

--- lib/arson/download.rb
+++ lib/arson/download.rb
@@ -1,16 +1,35 @@
 class Arson
-	# +package+ is expected to be a Hash, matching the structure the AUR
-	# RPC returns.
-	def download(package, depends=false)
-	end
+	class << self
+		# +package+ is expected to be a Hash, matching the structure the AUR
+		# RPC returns.
+		# TODO: Add automatic dependency tracking (gleaned parsing the PKGBUILD)
+		def download(package, depends=false)
+			begin
+				real_download("http://aur.archlinux.org"+package['URLPath'])
+			rescue Errno::EEXIST => e
+				warn e.message
+				exit 2
+			end
+		end
 
-	private
+		private
 
-	# The real downloading code, the parameter is expected to be a valid
-	# URL. No error checking is done.
-	#
-	# After the file is downloaded, it is attempted to be unpacked using
-	# tar and gunzip formats.
-	def real_download(url)
+		# The real downloading code, the parameter is expected to be a valid
+		# URL. No error checking is done.
+		#
+		# After the file is downloaded, it is attempted to be unpacked using
+		# tar and gunzip formats.
+		def real_download(url)
+			open(url) do |tar|
+				# Write the stream to a file, b is JIC
+				File.open(File.basename(url), "wb") do |file|
+					file.write(tar.read)
+				end
+			end
+			tgz = Zlib::GzipReader.new(File.open(File.basename(url), 'rb'))
+			# Extract pkg.tar.gz to `pwd`, instead of `pwd`/pkg
+			Archive::Tar::Minitar.unpack(tgz, Dir.pwd)
+			FileUtils.mv File.basename(url), File.join(Dir.pwd, File.basename(url)[0..-8])
+		end
 	end
 end
