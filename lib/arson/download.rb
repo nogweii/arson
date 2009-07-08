deac7ee6fe8e8d61e8b85bcf2c2be810c6a92916
Colin Shea <colin@evaryont.me>
2009-07-08 19:55:07 -0400

    Fix Arson::Config[] calls Signed-off-by: Colin Shea <colin@evaryont.me>
    These still used the old names ('dir' vs 'target_directory', for
    instance).

--- lib/arson/download.rb
+++ lib/arson/download.rb
@@ -8,7 +8,7 @@ class Arson
 			begin
 				real_download("http://aur.archlinux.org"+package['URLPath'])
 				if depends > 0
-					dependences = File.readlines("#{Arson::Config["dir"]}/#{package['Name']}/PKGBUILD").grep(/^(?:make)*depends/).map{|l| l.match(/.*=\((.*)\)$/)[1].gsub("'", '').split(' ')}.flatten.uniq.sort.map{|dep| (dep.scan(/(.*?)[><=]{1,2}(.*)/).first || [dep]).first }
+					dependences = File.readlines("#{Arson::Config["target_directory"]}/#{package['Name']}/PKGBUILD").grep(/^(?:make)*depends/).map{|l| l.match(/.*=\((.*)\)$/)[1].gsub("'", '').split(' ')}.flatten.uniq.sort.map{|dep| (dep.scan(/(.*?)[><=]{1,2}(.*)/).first || [dep]).first }
 					# Remove all those packages that are found in pacman's
 					# package database
 					dependences.reject { |depend| !is_sync?(depend) }
@@ -31,7 +31,7 @@ class Arson
 		# After the file is downloaded, it is attempted to be unpacked using
 		# tar and gunzip formats.
 		def real_download(url)
-			Dir.chdir(Arson::Config["dir"]) do
+			Dir.chdir(Arson::Config["target_directory"]) do
 				open(url) do |tar|
 					# Write the stream to a file, b (binary) is JIC
 					File.open(File.basename(url), "wb") do |file|
